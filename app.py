15c1456eca005e76280bfe996124d5f5b93e1c88
operacionemprender20-png <operacionemprender20@gmail.com>
2026-07-26 07:27:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,23 @@ REDIRECT_URI = "https://mercadolibre-callback.onrender.com/callback"
 # Almacenamiento temporal. Se pierde cuando Render reinicia.
 tokens = {}
 
+@app.route("/categorias", methods=["GET"])
+def categorias():
+    try:
+        categorias = obtener_categorias()
 
+        return jsonify(
+            status="ok",
+            total=len(categorias),
+            categorias=categorias
+        ), 200
+
+    except Exception as e:
+        return jsonify(
+            status="error",
+            message=str(e)
+        ), 500
+        
 @app.route("/", methods=["GET"])
 def inicio():
     return jsonify(
